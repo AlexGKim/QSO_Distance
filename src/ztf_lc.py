44a84ba032b64fd9f5eb4938864897afb0c05d79
Alex <agkim@lbl.gov>
2023-08-18 10:58:00 -0700

    select lcs only with relevant dates

--- src/ztf_lc.py
+++ src/ztf_lc.py
@@ -1,11 +1,12 @@
 import sys
 import io
 import requests
-import urllib
 import pandas
 import numpy
+import urllib
 from astropy.io import fits
 from astropy.io import ascii
+import matplotlib.pyplot as plt
 
 '''
 
@@ -20,6 +21,8 @@ Steps
 radius_arcsec = 0.5
 radius_degree = radius_arcsec/3600
 desistart = 59197   # Dec 14 2020 first night of iron
+
+lc_window = 5. # +/- days
 
 uploadFile = "upload.tbl"
 returnFile = "output.tbl"
@@ -53,18 +56,29 @@ def tableToObjects():
 
 def objectsToLCs():
 
+	# DESI information
+	fname = "/global/cfs/cdirs/desi/survey/catalogs/Y1/QSO/iron/QSO_cat_iron_main_dark_healpix_v0.fits"
+    hdul = fits.open(fname,memmap=True)
+
+    # ZTF information
 	data = ascii.read(returnFile)
 	df = data.to_pandas()
-	df = df[df['oid'].notna()]
+	df = df[df['oid'].notna()]	# prune out DESI objects not returned by ZTF
 
 	# query to IPAC Helpdesk says only one position per query available
+	counter = 0
 	for t in df.itertuples():
-		if (pandas.notna(t.oid)):
-			payload = {"ID": t.oid, "FORMAT": "CSV", "BAD_CATFLAGS_MASK": 32768}
-			r = requests.get('https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves', params=payload)
+        mjd_desi= hdul['QSO_CAT'].data['COADD_FIRSTMJD'][hdul['QSO_CAT'].data['TARGETID']== t.targetid_01][0]
+        payload = {"ID": t.oid, "FORMAT": "CSV", "BAD_CATFLAGS_MASK": 32768,"TIME":"{} {}".format(mjd_desi-lc_window, mjd_desi+lc_window)}
+        params = urllib.parse.urlencode(payload,quote_via=urllib.parse.quote)
+        r = requests.get('https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves', params=params)
+        ans = pandas.read_csv(io.StringIO(r.text))
+        if (not ans.empty):
 			with open("{}.tbl".format(t.targetid_01), "w") as f:
 				f.write(r.text)
-
+			count = count+1
+		if count ==1:
+			break
 
 if __name__ == '__main__':
     sys.exit(objectsToLCs())
