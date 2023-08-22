770bc4a06d43d583d58ed882d560de9e28fbffa5
Alex <agkim@lbl.gov>
2023-08-22 15:41:16 -0700

    works but slowwq

--- src/utils.py
+++ src/utils.py
@@ -8,11 +8,12 @@ import sqlalchemy
 import pandas.io.sql as sqlio
 import glob
 import pyarrow.parquet as pq
+import warnings
 
 conn_global = None
 df_global = None
+desi_global = None
 lc_dir='/pscratch/sd/a/akim/ZTF/irsa.ipac.caltech.edu/data/ZTF/lc/lc_dr18/[01]/'
-
 
 # DESI database
 def setup_conn():
@@ -30,33 +31,46 @@ def setup_df():
     df_global = data.to_pandas()
     df_global = df_global[df_global['oid'].notna()]  # prune out DESI objects not returned by ZTF
 
-
 # The database query is slow.  Downloaded the entire ZTF light curve data
 def targetidLC(targetid):
-
     if df_global is None: setup_df()
     df2 = df_global[df_global['targetid_01']==targetid]
     files = glob.glob(lc_dir+'field{0}/ztf_{0}_*_c{1}_q{2}_dr18.parquet'.format(df2.field.iloc[0].astype('str').zfill(6),df2.ccdid.iloc[0].astype('str').zfill(2),df2.qid.iloc[0]))
     ans=[]
     for f in files:
         df = pq.read_table(f).to_pandas()
-        ans.append(df.loc[df['objectid']==oid])
+        ans.append(df.loc[df['objectid']==df2.oid.iloc[0]])
 
     result = pandas.concat(ans, ignore_index=True, copy=False)
     return result
 
+# get the dates this was observed by DESI
 def targetidDESIMJDs(targetid):
-
-    # get the dates this was observed by DESI
+    global desi_global
     if conn_global is None: setup_conn()
     curr = """SELECT f.mjd, f.night
              FROM iron.healpix_expfibermap f
-             WHERE targetid={} AND fiberstatus=0""".format(targetid)
+             WHERE targetid={}""".format(targetid)
     desi_df = sqlio.read_sql_query(curr, conn_global)
     desi_df = desi_df.groupby(['night']).mean().reset_index()
+
+    # with warnings.catch_warnings(record=True) as w:
+    #     warnings.simplefilter("always")
+    #     desi_df = desi_df.groupby(['night']).mean().reset_index()
+    #     if len(w):
+    #         print(desi_df['night'])
+    #         print(targetid)
+    #         wef
+
     return desi_df
 
-
+def trimLC(lc, dates, window):
+    index = numpy.full(lc.nepochs, False)
+    hmjd = lc['hmjd'].values[0]
+    for date in dates.itertuples():
+        index = numpy.logical_or(index, numpy.abs(hmjd-date.mjd) < window/2)
+    return hmjd[index], lc['magerr'].values[0][index], lc['mag'].values[0][index]
+ 
     # # query to IPAC Helpdesk says only one position per query available
     # globalcounter=0
     # for t in df.itertuples():
@@ -79,7 +93,22 @@ def targetidDESIMJDs(targetid):
     #                 f.write(r.text)
     #     globalcounter=globalcounter+1
 
+def main():
+    if df_global is None: setup_df()
+    print("number of targetids {}".format(df_global.shape[0]))
+    ans={1:0, 3:0, 5:0, 7:0, 9:0}
+    count=0
+    for row in df_global.itertuples():
+        if (count % 10000 ==0): print(count)
+        # targetid = 39627322701128888
+        lc = targetidLC(row.targetid_01)
+        dates = targetidDESIMJDs(row.targetid_01)
+        for k in ans.keys():
+            dum = trimLC(lc,dates,k)
+            ans[k] = ans[k]+len(dum[0])
+        count += 1
+
+    print(ans)
 
 if __name__ == '__main__':
-    sys.exit(targetidDESIMJDs(39627322701128888))
-    #sys.exit(targetidLC(39627322701128888))
+    sys.exit(main())
